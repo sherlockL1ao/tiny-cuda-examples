37a64ad65ad92f3c40baf9513774c79c1b870c80
sherlock <sherlock.liao.2@gmail.com>
2025-11-24 21:50:08 +0800

    feat(gemv): implement optimized GEMV kernels with warp reduction
    - Add warpReduceSum function for efficient reduction in SgemvK32 kernel.
    - Refactor gemv_launcher to select between SgemvK32 and SgemvNaiveKernel
      based on input dimensions.

--- gemv/gemv.cuh
+++ gemv/gemv.cuh
@@ -1,12 +1,30 @@
 #include <cuda_runtime.h>
 
 #define TILE_SIZE 32
-#define SMEM_B_SIZE 128
+
+template <unsigned int warpSize> __device__ __forceinline__ float warpReduceSum(float sum) {
+  if (warpSize >= 32) {
+    sum += __shfl_down_sync(0xffffffff, sum, 16); // 0-16, 1-17, ...
+  }
+  if (warpSize >= 16) {
+    sum += __shfl_down_sync(0xffffffff, sum, 8); // 0-8, 1-9, ...
+  }
+  if (warpSize >= 8) {
+    sum += __shfl_down_sync(0xffffffff, sum, 4); // 0-4, 1-5, ...
+  }
+  if (warpSize >= 4) {
+    sum += __shfl_down_sync(0xffffffff, sum, 2); // 0-2, 1-3, ...
+  }
+  if (warpSize >= 2) {
+    sum += __shfl_down_sync(0xffffffff, sum, 1); // 0-1
+  }
+  return sum;
+}
 
 /*
 A: [m, k], B:[1, k], C: [m, 1]
 */
-__global__ void naive_sgemv_kernel(const float *A, const float *B, float *C, int m, int k) {
+__global__ void SgemvNaiveKernel(const float* __restrict__ A, const float* __restrict__ B, float* C, int m, int k) {
   int rows = blockIdx.x * blockDim.x + threadIdx.x;
   if (rows < m) {
     int a_idx = rows * k;
@@ -18,10 +36,14 @@ __global__ void naive_sgemv_kernel(const float *A, const float *B, float *C, int
   }
 }
 
-__global__ void smemGEMVKernel(const float *A, const float* B, float *C, int m, int k) {
-  __shared__ float smemB[SMEM_B_SIZE];
-  int rows = blockIdx.x * blockDim.x + threadIdx.x;
+__global__ void SgemvK32(const float* __restrict__ A, const float* __restrict__ B, float* C, int m, int k) {
+  int rows = blockIdx.x * blockDim.y + threadIdx.y;
   if (rows < m) {
-
+    int   a_idx = rows * k;
+    float sum = A[a_idx + threadIdx.x] * B[threadIdx.x];
+    sum = warpReduceSum<32>(sum);
+    if (threadIdx.x == 0) {
+      C[rows] = sum;
+    }
   }
 }

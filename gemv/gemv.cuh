83bc5bbb240e3f6e3ab298bb0e46c313ca3631e4
sherlock <sherlock.liao.2@gmail.com>
2025-11-25 11:03:17 +0800

    feat(gemv): add SgemvK16 kernel for GEMV execution

--- gemv/gemv.cuh
+++ gemv/gemv.cuh
@@ -3,12 +3,12 @@
 #define TILE_SIZE 32
 #define WARP_SIZE 32
 
-template <unsigned int wrap_size> __device__ __forceinline__ float warpReduceSum(float sum) {
-  if (wrap_size >= 32) sum += __shfl_down_sync(0xffffffff, sum, 16); // 0-16, 1-17, ...
-  if (wrap_size >= 16) sum += __shfl_down_sync(0xffffffff, sum, 8);  // 0-8, 1-9, ...
-  if (wrap_size >= 8) sum += __shfl_down_sync(0xffffffff, sum, 4);   // 0-4, 1-5, ...
-  if (wrap_size >= 4) sum += __shfl_down_sync(0xffffffff, sum, 2);   // 0-2, 1-3, ...
-  if (wrap_size >= 2) sum += __shfl_down_sync(0xffffffff, sum, 1);   // 0-1
+template <unsigned int warp_size> __device__ __forceinline__ float warpReduceSum(float sum) {
+  if (warp_size >= 32) sum += __shfl_down_sync(0xffffffff, sum, 16); // 0-16, 1-17, ...
+  if (warp_size >= 16) sum += __shfl_down_sync(0xffffffff, sum, 8);  // 0-8, 1-9, ...
+  if (warp_size >= 8) sum += __shfl_down_sync(0xffffffff, sum, 4);   // 0-4, 1-5, ...
+  if (warp_size >= 4) sum += __shfl_down_sync(0xffffffff, sum, 2);   // 0-2, 1-3, ...
+  if (warp_size >= 2) sum += __shfl_down_sync(0xffffffff, sum, 1);   // 0-1
   return sum;
 }
 
@@ -45,13 +45,13 @@ __global__ void SgemvK128(const float* __restrict__ A, const float* __restrict__
   int rows = blockIdx.x * blockDim.y + threadIdx.y;
   int lane = threadIdx.x; // 0..31
   if (rows >= m) return;
-  int a_idx = rows*k;
+  int   a_idx = rows * k;
   float sum = 0;
-  int kIteration = k / (WARP_SIZE*4);
+  int   kIteration = k / (WARP_SIZE * 4);
 #pragma unroll
   for (int i = 0; i < kIteration; ++i) {
-    int col_idx = (i * WARP_SIZE + lane) * 4;
-    const float4* a_vals = reinterpret_cast<const float4*>(A + a_idx + col_idx);
+    int           col_idx = (i * WARP_SIZE + lane) * 4;
+    const float4* a_vals = reinterpret_cast<const float4*>(A + a_idx + col_idx); // load 4 floats
     const float4* b_vals = reinterpret_cast<const float4*>(B + col_idx);
     sum += a_vals->x * b_vals->x;
     sum += a_vals->y * b_vals->y;
@@ -60,4 +60,15 @@ __global__ void SgemvK128(const float* __restrict__ A, const float* __restrict__
   }
   sum = warpReduceSum<32>(sum);
   if (lane == 0) C[rows] = sum;
+}
+
+__global__ void SgemvK16(const float* __restrict__ A, const float* __restrict__ B, float* C, int m, int k) {
+  int rows = (blockIdx.x * blockDim.y + threadIdx.y) * 2;
+  int lane = threadIdx.x; // 0..31
+  if (rows >= m) return;
+  int a_idx = rows * k;
+  float sum = A[a_idx + lane] * B[lane % 16];
+  sum = warpReduceSum<16>(sum);
+  if (lane == 0) C[rows] = sum;
+  if (lane == 16) C[rows + 1] = sum;
 }

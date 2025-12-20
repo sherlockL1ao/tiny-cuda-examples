2701181264e8b1d55b2537213b6d606bbee71b9a
sherlock <sherlock.liao.2@gmail.com>
2025-12-20 23:47:01 +0800

    refactor(nvfp4_gemv): update test parameters and add reg tile

--- nvfp4_gemv/test_nvfp4_gemv.py
+++ nvfp4_gemv/test_nvfp4_gemv.py
@@ -1,5 +1,5 @@
 import torch
-from nvfp4_gemv_inline import nvfp4_gemv_asm, nvfp4_gemv_asm_warp, nvfp4_gemv_asmv2, nvfp4_gemv_naive
+from nvfp4_gemv_inline import nvfp4_gemv_asm, nvfp4_gemv_asm_warp, nvfp4_gemv_asmv2, nvfp4_gemv_naive, nvfp4_gemv_reg_tile
 from nvfp4_gemv_reference import generate_input, ref_kernel
 from utils import make_match_reference
 
@@ -34,14 +34,14 @@ def time_pytorch_function(func, data):
   return start.elapsed_time(end) # Return the elapsed time in milliseconds
 
 
-m, k, l = 128, 512, 4
+m, k, l = 256, 1024, 8
 
 data_ref = generate_input(m=m, k=k, l=l, seed=42)
 # ref_out = ref_kernel(data_ref)
 ref_out = nvfp4_gemv_asmv2(data_ref)
 
 data_cuda = generate_input(m=m, k=k, l=l, seed=42)
-check_impl = make_match_reference(nvfp4_gemv_asm_warp, rtol=1e-03, atol=1e-03)
+check_impl = make_match_reference(nvfp4_gemv_reg_tile, rtol=1e-03, atol=1e-03)
 matched, msg = check_impl(data_cuda, ref_out)
 
 if not matched:
@@ -49,18 +49,20 @@ if not matched:
     exit()
 
 
-for m, k in ((2048, 512), (512, 2048), (8192, 8192)):
-    data = generate_input(m=m, k=k, l=32, seed=42)
+for m, k in ((2048, 1024), (512, 2048), (4096, 8192), (512, 16384*2)):
+    data = generate_input(m=m, k=k, l=64, seed=42)
     torch_time = time_pytorch_function(ref_kernel, data)
     naive_time = time_pytorch_function(nvfp4_gemv_naive, data)
     asm_time = time_pytorch_function(nvfp4_gemv_asm, data)
     asmv2_time = time_pytorch_function(nvfp4_gemv_asmv2, data)
     asm_warp_time = time_pytorch_function(nvfp4_gemv_asm_warp, data)
+    reg_tile_time = time_pytorch_function(nvfp4_gemv_reg_tile, data)
     print(
         f"m={m}, k={k} | "
         f"PyTorch: {torch_time:.2f} ms | "
         f"Naive: {naive_time:.2f} ms | "
         f"ASM: {asm_time:.2f} ms | "
         f"ASMV2: {asmv2_time:.2f} ms | "
-        f"ASM warp: {asm_warp_time:.2f} ms"
+        f"ASM warp: {asm_warp_time:.2f} ms | "
+        f"Reg tile: {reg_tile_time:.2f} ms"
     )

049efd3ec1aa1fd3236c97656f8cca6c679fdf5a
sherlock <sherlock.liao.2@gmail.com>
2026-01-01 19:00:55 +0800

    build(sgemm): update extra CUDA compilation flags

--- sgemm/sgemm_inline.py
+++ sgemm/sgemm_inline.py
@@ -25,7 +25,7 @@ def build_extension(cuda_path: Path, module_name: str, verbose: bool = True) -> 
         name=module_name,
         cpp_sources=[cpp_source],
         cuda_sources=[cuda_path.read_text()],
-        extra_cuda_cflags=["-gencode=arch=compute_120a,code=sm_120a", "--expt-relaxed-constexpr"],
+        extra_cuda_cflags=["-gencode=arch=compute_120a,code=sm_120a", "--expt-relaxed-constexpr"],# "--ptxas-options=-v"],
         verbose=verbose,
     )
 
